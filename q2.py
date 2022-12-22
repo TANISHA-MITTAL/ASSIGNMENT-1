a4e0ebb34876fa56578c9100c35cea38693e56e2
TANISHA-MITTAL <121186664+TANISHA-MITTAL@users.noreply.github.com>
2022-12-22 12:24:36 +0530

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,7 @@
+GrossIncome = int(input("Enter your Gross Income  "))
+Dependents = int(input("Enter the number of Dependents- "))
+
+Tax_1 = GrossIncome-10000-(Dependents*3000)
+Final_Tax = Tax_1*0.2
+
+print("Your tax is $ ",Final_Tax)
---
+++ q4.py
@@ -0,0 +1,5 @@
+a = 25
+b = 25.0
+c = 25
+FinalOutput = str(a + int(b) + int(c))
+print(FinalOutput)
